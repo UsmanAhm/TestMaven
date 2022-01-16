3617a2ffc0c443c1c8ee5d6c6772947417f2dc12
usman <mail@usmanahmad.de>
2022-01-16 23:20:05 +0100

    version2

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -4,7 +4,30 @@ class CalculatorTest {
 
     @org.junit.jupiter.api.Test
     void multiply() {
-        assertEquals(20, Calculator.multiply(4, 5),
-                "Regular multiplication should work");
+        assertEquals(15, Calculator.multiply(3, 5), "Regular multiplication should work");
+        assertEquals(25, Calculator.multiply(5, 5), "Regular multiplication should work");
+        assertEquals(20, Calculator.multiply(4, 5), "Regular multiplication should work");
+       // assertEquals(20, Calculator.multiply(3, 5), "Regular multiplication should work");
     }
+
+    @org.junit.jupiter.api.Test
+    void multiply1() {
+
+        assertEquals(25, Calculator.multiply(5, 5), "Regular multiplication should work");
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void multiply2() {
+
+        assertEquals(20, Calculator.multiply(4, 5), "Regular multiplication should work");
+        // assertEquals(20, Calculator.multiply(3, 5), "Regular multiplication should work");
+    }
+
+    @org.junit.jupiter.api.Test
+    void multiply3() {
+        assertEquals(20, Calculator.multiply(3, 5), "Regular multiplication should work");
+    }
+
+
 }
